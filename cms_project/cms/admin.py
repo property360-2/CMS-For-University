d86a53f3b718a20cca4b5960fdb8e5957604d620
Jun Alvior <junalvior21@gmail.com>
2025-10-11 11:54:22 +0800

    reading feedbacks

--- cms_project/cms/admin.py
+++ cms_project/cms/admin.py
@@ -1,18 +1,67 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.utils import timezone
+from datetime import timedelta
 from .models import Page
 
-@admin.register(Page)
+# Custom Admin Site
+class CustomAdminSite(AdminSite):
+    site_header = 'MarkCMS Administration'
+    site_title = 'MarkCMS Admin'
+    index_title = 'Dashboard'
+    
+    def index(self, request, extra_context=None):
+        # Add statistics to context
+        extra_context = extra_context or {}
+        
+        # Calculate statistics
+        total_pages = Page.objects.count()
+        published_pages = Page.objects.filter(is_published=True).count()
+        
+        # Pages created in last 7 days
+        seven_days_ago = timezone.now() - timedelta(days=7)
+        recent_pages = Page.objects.filter(created_at__gte=seven_days_ago).count()
+        
+        extra_context.update({
+            'total_pages': total_pages,
+            'published_pages': published_pages,
+            'recent_pages': recent_pages,
+        })
+        
+        return super().index(request, extra_context)
+
+# Create custom admin site instance
+admin_site = CustomAdminSite(name='custom_admin')
+
+@admin.register(Page, site=admin_site)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'is_published', 'created_at', 'updated_at')
-    list_filter = ('is_published', 'created_at')
-    search_fields = ('title', 'content_md')
+    list_filter = ('is_published', 'created_at', 'updated_at')
+    search_fields = ('title', 'content_md', 'seo_description', 'seo_keywords')
     prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    list_per_page = 25
+    
     fieldsets = (
         ('Content', {
-            'fields': ('title', 'slug', 'content_md', 'is_published')
+            'fields': ('title', 'slug', 'content_md', 'is_published'),
+            'description': 'Main content of your page. Use Markdown syntax for formatting.'
         }),
-        ('SEO', {
+        ('SEO & Meta', {
             'fields': ('seo_description', 'seo_keywords'),
-            'classes': ('collapse',)
+            'classes': ('collapse',),
+            'description': 'Optimize your page for search engines.'
         }),
     )
+    
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)
+        }
+
+# Register built-in models with custom admin site
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+
+admin_site.register(User, UserAdmin)
+admin_site.register(Group, GroupAdmin)
--- cms_project/cms_project/urls.py
+++ cms_project/cms_project/urls.py
@@ -1,14 +1,14 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from cms.sitemaps import PageSitemap
+from cms.admin import admin_site  # Import custom admin site
 
 sitemaps = {
     'pages': PageSitemap,
 }
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),  # Use custom admin site
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('', include('cms.urls')),
 ]
